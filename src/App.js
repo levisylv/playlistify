10642586dbb81d8a3f8da39a22f3b42a7359e035
LeviSylv <levisylv@amazon.com>
2019-09-04 12:37:20 -0700

    error fix

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Playlistify</h1>
-      <Container>
-        <Row>
-          <Col>Spotify</Col>
-          <Col>Itunes</Col>
-          <Col>Google Play Music</Col>
-        </Row>
-      </Container>
+      <header className="App-header">
+        <h1>Playlistify</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
