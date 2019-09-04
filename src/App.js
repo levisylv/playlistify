03f5ffe78ef066119f05a199c77dd695ced2b60a
LeviSylv <levisylv@amazon.com>
2019-09-04 12:17:42 -0700

    music providers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>Playlistify</h1>
+      <Container>
+        <Row>
+          <Col>Spotify</Col>
+          <Col>Itunes</Col>
+          <Col>Google Play Music</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
