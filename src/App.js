b1cae577a02f9d08ec2ea2db9697dc22eaf8a226
LeviSylv <levisylv@amazon.com>
2019-09-04 11:32:00 -0700

    Blank start

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>You've made it</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Playlistify</h1>
     </div>
   );
 }
