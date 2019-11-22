ead7643b8e3c9e3da16d32ba9427b625e043d5f0
LeviSylv <levisylv@amazon.com>
2019-11-22 11:58:32 -0800

    fixing redirect

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,12 +16,25 @@ import PlayList from './PlayList/PlayList';
 import * as SpotifyWebApi from 'spotify-web-api-js';
 
 let defaultStyle = {
-  color: '#fff'
+  color: '#fff',
+  'font-family': 'Papyrus'
 };
+let counterStyle = {...defaultStyle, 
+  width: "40%", 
+  display: 'inline-block',
+  'margin-bottom': '20px',
+  'font-size': '20px',
+  'line-height': '30px'
+}
+function isEven(number) {
+  return number % 2
+}
+
 class PlaylistCounter extends Component {
   render() {
+    let playlistCounterStyle = counterStyle
     return (
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+      <div style={playlistCounterStyle}>
         <h2>{this.props.playlists.length} playlists</h2>
       </div>
     );
@@ -36,9 +49,15 @@ class HoursCounter extends Component {
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
     }, 0)
+    let totalDurationHours = Math.round(totalDuration/60)
+    let isTooLow = totalDurationHours < 40
+    let hoursCounterStyle = {...counterStyle, 
+      color: isTooLow ? 'red' : 'white',
+      'font-weight': isTooLow ? 'bold' : 'normal',
+    }
     return (
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-        <h2>{Math.round(totalDuration/60)} hours</h2>
+      <div style={hoursCounterStyle}>
+        <h2>{totalDurationHours} hours</h2>
       </div>
     );
   }
@@ -50,7 +69,11 @@ class Filter extends Component {
       <div style={defaultStyle}>
         <img/>
         <input type="text" onKeyUp={event => 
-          this.props.onTextChange(event.target.value)}/>
+          this.props.onTextChange(event.target.value)}
+          style={{...defaultStyle, 
+            color: 'black', 
+            'font-size': '20px', 
+            padding: '10px'}}/>
       </div>
     );
   }
@@ -60,23 +83,29 @@ class Playlist extends Component {
   render() {
     let playlist = this.props.playlist
     return (
-      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
+      <div style={{...defaultStyle, 
+        display: 'inline-block', 
+        width: "25%",
+        padding: '10px',
+        'background-color': isEven(this.props.index) 
+          ? '#C0C0C0' 
+          : '#808080'
+        }}>
+        <h2>{playlist.name}</h2>
         <img src={playlist.imageUrl} style={{width: '60px'}}/>
-        <h3>{playlist.name}</h3>
-        <ul>
+        <ul style={{'margin-top': '10px', 'font-weight': 'bold'}}>
           {playlist.songs.map(song => 
-            <li>{song.name}</li>
+            <li style={{'padding-top': '2px'}}>{song.name}</li>
           )}
         </ul>
       </div>
     );
   }
-}    
+} 
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.spotifyApi = new SpotifyWebApi();
     this.state = {
       serverData: {},
       filterString: '',
@@ -205,16 +234,40 @@ export default class Home extends Component {
       fetch('https://api.spotify.com/v1/me/playlists', {
         headers: {'Authorization': 'Bearer ' + _token}
       }).then(response => response.json())
-      .then(data => this.setState({
-        playlists: data.items.map(item => {
-          console.log(data.items)
+      .then(playlistData => {
+        let playlists = playlistData.items
+        let trackDataPromises = playlists.map(playlist => {
+          let responsePromise = fetch(playlist.tracks.href, {
+            headers: {'Authorization': 'Bearer ' + _token}
+          })
+          let trackDataPromise = responsePromise
+            .then(response => response.json())
+          return trackDataPromise
+        })
+        let allTracksDataPromises = 
+          Promise.all(trackDataPromises)
+        let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+          trackDatas.forEach((trackData, i) => {
+            playlists[i].trackDatas = trackData.items
+              .map(item => item.track)
+              .map(trackData => ({
+                name: trackData.name,
+                duration: trackData.duration_ms / 1000
+              }))
+          })
+          return playlists
+        })
+        return playlistsPromise
+      })
+      .then(playlists => this.setState({
+        playlists: playlists.map(item => {
           return {
             name: item.name,
             imageUrl: item.images[0].url, 
-            songs: []
+            songs: item.trackDatas.slice(0,3)
           }
       })
-      })) 
+      }))
     }
   }
 
@@ -274,33 +327,40 @@ export default class Home extends Component {
 
   renderPlaylists() {
     let playlistToRender = 
-      this.state.user && 
-      this.state.playlists 
-        ? this.state.playlists.filter(playlist => 
-          playlist.name.toLowerCase().includes(
-            this.state.filterString.toLowerCase())) 
-        : []
+    this.state.user && 
+    this.state.playlists 
+      ? this.state.playlists.filter(playlist => {
+        let matchesPlaylist = playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase()) 
+        let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+          .includes(this.state.filterString.toLowerCase()))
+        return matchesPlaylist || matchesSong
+      }) : []
     return (
-      <div className="App">
-      {this.state.user ?
-      <div>
-        <h1 style={{...defaultStyle, 'font-size': '54px'}}>
-          {this.state.user.name}'s Playlists
-        </h1>
-        <Filter onTextChange={text => {
-            this.setState({filterString: text})
-          }}/>
-        {playlistToRender.map(playlist => 
-          <Playlist playlist={playlist} />
-        )}
-      </div> 
-      : <button onClick={() => {
-          window.location = window.location.href.includes('localhost') 
-            ? 'http://localhost:8888/login' 
-            : 'https://playlistify.levisylv.com/login' }
+<div className="App">
+        {this.state.user ?
+        <div>
+          <h1 style={{...defaultStyle, 
+            'font-size': '54px',
+            'margin-top': '5px'
+          }}>
+            {this.state.user.name}'s Playlists
+          </h1>
+          <PlaylistCounter playlists={playlistToRender}/>
+          <HoursCounter playlists={playlistToRender}/>
+          <Filter onTextChange={text => {
+              this.setState({filterString: text})
+            }}/>
+          {playlistToRender.map((playlist, i) => 
+            <Playlist playlist={playlist} index={i} />
+          )}
+        </div> : <button onClick={() => {
+            window.location = window.location.href.includes('localhost') 
+              ? 'http://localhost:8888/login' 
+              : 'https://playlistify.levisylv.com/CurrentlyPlaying' }
+          }
+          style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
         }
-        style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
-      }
     
       <div className="playlists">
         <PageHeader>Your Playlists</PageHeader>
